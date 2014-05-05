40bbfabca2995db3c5b8f4cb88aa9d3e20727df3
tao.pei <tao.pei@ck-telecom.com>
2014-05-05 21:03:54 +0800

    dlg

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
-
+#include "logindialog.h"
+#include <QDesktopWidget>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-    
-    return a.exec();
+    LoginDialog loginDlg;
+    QDesktopWidget *desk=QApplication::desktop();
+
+    if(loginDlg.exec() == QDialog::Accepted)
+    {
+        w.move((desk->width() - w.width())/2,
+                   (desk->height() - w.height())/2);
+        w.show();
+        return a.exec();
+    }
+    else
+    {
+        return 0;
+    }
 }
